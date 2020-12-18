4875778335faf3ee63170f3ac71ea169205dee31
DrBracewell <dragonclawslice1@gmail.com>
2020-12-18 20:26:58 +0000

    changed emoji

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ import * as config from "./json/config.json"
 export const PREFIX = config.prefix;
 export const MANSION = config.servers.mansion;
 export const CTCODING = config.servers.programming;
-export const OPEN_EMOJI = "🅾️";
+export const OPEN_EMOJI = "⭕";
 export const DEVELOPMENT = process.env.NODE_ENV === "development";
