dfd4fd53f1e5e66a53780f0a0785e536dcac7170
DrBracewell <dragonclawslice1@gmail.com>
2020-12-21 16:32:07 +0000

    added extra check for closescene

--- src/commands/closescene.ts
+++ src/commands/closescene.ts
@@ -1,4 +1,5 @@
-import { Client, Message} from "discord.js";
+import { Client, Message, TextChannel} from "discord.js";
+import { OPEN_EMOJI } from "../constants";
 import { sceneMap } from "../controllers/openscenes";
 
 module.exports = {
@@ -7,7 +8,12 @@ module.exports = {
     description: "Mark a currently open channel as closed by removing the emoji in the name.",
     arguments: "",
     execute(client: Client, message: Message, args: string[]) {
-        if (!sceneMap.has(message.channel.id)) {
+        const channel = message.channel as TextChannel;
+        if (!sceneMap.has(channel.id)) {
+            if (channel.name.includes(`${OPEN_EMOJI}-`)) {
+                channel.setName(channel.name.replace(`${OPEN_EMOJI}-`, ""));
+                return;
+            }
             message.reply("That channel is not marked as an open scene!");
             return;
         }
