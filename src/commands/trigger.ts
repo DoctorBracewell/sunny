e76275bc43383f54669e33f58317c74251dbcf54
DrBracewell <dragonclawslice1@gmail.com>
2021-04-03 18:24:59 +0100

    small fixes

--- src/commands/trigger.ts
+++ src/commands/trigger.ts
@@ -1,15 +1,20 @@
 import { Client, Message } from "discord.js";
+import { MOD_ROLE } from "../constants";
 
 export const command = {
-    name: "trigger",
-    category: "utility",
-    description: "For mod use only, triggers certain custom-coded events.",
-    arguments: "<varied>",
-    async execute(client: Client, message: Message, args: string[]) {
-        if (!message.guild.member(message.author).roles.cache.find(role => role.name === "MOD")) return message.reply("this trigger can only be run by a moderator!");
+  name: "trigger",
+  category: "utility",
+  description: "For mod use only, triggers certain custom-coded events.",
+  arguments: "<varied>",
+  async execute(client: Client, message: Message, args: string[]) {
+    if (
+      !message.guild
+        .member(message.author)
+        .roles.cache.find((role) => role.name === MOD_ROLE)
+    )
+      return message.reply("this trigger can only be run by a moderator!");
 
-        
-        /* Add roles for everyone
+    /* Add roles for everyone
         client.guilds.cache.get("612778224887267342").members.cache.forEach(async member => {
             if (member.roles.cache.find(role => role.name === "Blocked Channels")) return;
             if (member.user.bot) return;
@@ -20,5 +25,5 @@ export const command = {
 
             console.log(`Added roles for: ${member.user.name}`);
         })*/
-    }
-}
+  },
+};
